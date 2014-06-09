dbd52469d9360124c42bf03db2bddde510c0528c
Justin Page <xjustinpagex@gmail.com>
2014-06-09 16:44:19 -0700

    added exercise three

--- exc_2/ex2.rb
+++ exc_2/ex2.rb
@@ -5,4 +5,4 @@ puts "I have code like this." # and the comment after is ignored
 # you can also use a comment to "disable" or comment out a piece of code:
 # puts "this won't run."
 
-puts "This will run."
+puts "This will run.".reverse
